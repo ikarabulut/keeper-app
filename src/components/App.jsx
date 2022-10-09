1b7a15cde1b0f2125fb0720764a761b6c8874d36
Ismail Karabulut <i.karabulut93@gmail.com>
2022-10-09 18:24:00 -0500

    Update prettier to use double quote and format

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import Note from './Note'
+import React from "react"
+import Header from "./Header"
+import Footer from "./Footer"
+import Note from "./Note"
 
 function App() {
   return (
