332fefc0e4d14c594acb6c3180ef4528ef5643c7
Ismail Karabulut <i.karabulut93@gmail.com>
2022-10-09 22:31:58 -0500

    Extract HomePage and login page to user flow

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,11 @@
-import React, { useState } from "react"
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
-import CreateArea from "./CreateArea"
+import React from "react"
+import HomePage from "./HomePage"
+import Login from "./Login"
 
 function App() {
-  const [notes, setNotes] = useState([])
+  let isLoggedIn = true
 
-  function addNote(newNote) {
-    setNotes((prevNotes) => {
-      return [...prevNotes, newNote]
-    })
-  }
-
-  function deleteNote(id) {
-    setNotes((prevNotes) => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id
-      })
-    })
-  }
-
-  return (
-    <div>
-      <Header />
-      <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => (
-        <Note
-          key={index}
-          id={index}
-          title={noteItem.title}
-          content={noteItem.content}
-          onDelete={deleteNote}
-        />
-      ))}
-
-      <Footer />
-    </div>
-  )
+  return isLoggedIn ? <HomePage /> : <Login />
 }
 
 export default App
