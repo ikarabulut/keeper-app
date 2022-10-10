1e01f814cdcd6a9c21f832e710a8037b8d34ae7e
Ismail Karabulut <i.karabulut93@gmail.com>
2022-10-10 14:11:32 -0500

    Add login event handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,63 @@
-import React from "react"
-import Input from "./Input"
+import React, { useState } from "react"
+// import Input from "./Input"
 import { useNavigate } from "react-router-dom"
 
 function Login() {
   const navigate = useNavigate()
 
-  function navigateToHome() {
-    navigate("/home")
+  const validCredentials = {
+    username: "test",
+    password: "password",
+  }
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [loginError, setLoginError] = useState(false)
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value)
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value)
+  }
+
+  function navigateToHome(event) {
+    if (
+      username === validCredentials.username &&
+      password === validCredentials.password
+    ) {
+      navigate("/home")
+    } else {
+      setLoginError(true)
+      setUsername("")
+      setPassword("")
+      event.preventDefault()
+    }
   }
   return (
     <div className="container">
       <h1>Hello</h1>
       <p>Please log-in</p>
       <form className="form">
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
+        <input
+          onChange={handleUsernameChange}
+          type="text"
+          placeholder="Username"
+          value={username}
+        />
+        <input
+          onChange={handlePasswordChange}
+          type="password"
+          placeholder="Password"
+          value={password}
+        />
         <button onClick={navigateToHome}>Login</button>
       </form>
+      {loginError && (
+        <h2 id="loginError">Incorrect Credentials, Please try again</h2>
+      )}
     </div>
   )
 }
